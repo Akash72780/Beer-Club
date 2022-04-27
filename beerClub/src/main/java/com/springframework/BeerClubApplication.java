65697bc288cdf0ce97248962928e34913e6ef5a0
Windows 7 <Windows 7@Windows7-PC>
2022-04-27 18:28:50 +0530

    start a project

---
+++ beerClub/src/main/java/com/springframework/BeerClubApplication.java
@@ -0,0 +1,13 @@
+package com.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerClubApplication.class, args);
+	}
+
+}
